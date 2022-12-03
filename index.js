6c9e0828a2a7fcb3366b55db0b3280ed673b4e6c
Turtwi <210103367@stu.sdu.edu.kz>
2022-12-03 23:05:45 +0600

    Added mobile adaptation

---
+++ index.js
@@ -0,0 +1,6 @@
+function toggle(){
+    document.getElementById('nav').classList.toggle('navactive')
+}
+function search(){
+    document.getElementById('searchThing').classList.toggle('searchactive')
+}
